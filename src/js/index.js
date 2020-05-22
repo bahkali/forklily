3d52c7b81186cfe048f8f1679ee9cb171cbb082c
kaly <kalimamadou@gmail.com>
2020-05-22 16:42:27 -0700

    Setup webpack and babel and layout of project file

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import num from "./test";
+const x = 23;
+console.log(
+  `I imported ${num} from another module called text.js Variable x is ${x}`
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: ["@babel/polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "./dist"),
+    port: 8000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
