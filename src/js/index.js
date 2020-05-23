83007c54e3653c047ec3c40508b43b891cb4d8f1
kaly <kalimamadou@gmail.com>
2020-05-22 19:57:39 -0700

    added functionality to get input from search and render the result in the list

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from "./modules/search.module";
-
+import * as searchView from "./views/search.view";
+import { elements } from "./views/base";
 import { queryArray } from "./modules/search-query";
 /**
  * Global State of the app
@@ -10,21 +11,21 @@ import { queryArray } from "./modules/search-query";
 const state = {};
 const controlSearch = async () => {
   //get query from UI
-  const query = queryArray[1];
-
+  const query = searchView.getInput();
   if (query) {
     //new search object and add to state
     state.search = new Search(query);
     //prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     //search for recipes
     await state.search.getResult();
     //render to UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
-
-document.querySelector(".search").addEventListener("submit", (event) => {
+elements.searchForm.addEventListener("submit", (event) => {
   event.preventDefault(); // prevent reload
   controlSearch();
 });
