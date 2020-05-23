21ed37300eaa6d2ea667cde466fd39b10e9246cf
kaly <kalimamadou@gmail.com>
2020-05-22 19:18:22 -0700

    Partial build search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 import Search from "./modules/search.module";
 
-const search = new Search("pasta");
-console.log(search);
-search.getResult("pasta");
+import { queryArray } from "./modules/search-query";
+/**
+ * Global State of the app
+ * - Search object
+ * -Current recipe object
+ * - Liked recipes
+ */
+const state = {};
+const controlSearch = async () => {
+  //get query from UI
+  const query = queryArray[1];
+
+  if (query) {
+    //new search object and add to state
+    state.search = new Search(query);
+    //prepare UI for results
+
+    //search for recipes
+    await state.search.getResult();
+    //render to UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent reload
+  controlSearch();
+});
--- src/js/modules/search.module.js
+++ src/js/modules/search.module.js
@@ -1,5 +1,6 @@
 // const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
 //const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+// get https://forkify-api.herokuapp.com/api/get  https://forkify-api.herokuapp.com/api/get?rId=47746
 import axios from "axios";
 
 export default class Search {
